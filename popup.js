7d715ed61f8a58bf326684535491ef2571112ae3
jinpyojeon <jinpyo.jeon@trincoll.edu>
2017-02-20 11:16:09 -0500

    added most basic feature

--- popup.js
+++ popup.js
@@ -1,50 +1,71 @@
 // Modeled this on an example from Google to show kittens.
 
-var QUERY = 'bees';
+var username; // remove soon
+
+function saveUsername() {
+	var username = document.getElementByName("username");
+	username = username.textContent;
+}
 
 var beeGenerator = {
-  searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
-                   'method=flickr.photos.search&' +
-                   'api_key=90485e931f687a9b9c2a66bf58a3861a&' +
-                   'text=' + encodeURIComponent(QUERY) + '&' +
-                   'safe_search=0&' +
-                   'content_type=1&' +
-                   'sort=interestingness-desc&' +
-                   'per_page=20',
 
-  // Sends an XHR GET request to grab photos. The XHR's 'onload' event is 
-  // hooked up to the 'showPhotos_' method.
-  requestBees: function() {
-    var req = new XMLHttpRequest();
-    req.open("GET", this.searchOnFlickr_, true);
-    req.onload = this.showPhotos_.bind(this);
-    req.send(null);
-  },
+	requestDangerGoals_: 'https://beeminder.com/api/v1' + 
+						 '/users/aba1731.json' +
+						 '?auth_token=R7qosphRUcKzV6Y36h4p'
+	,
 
-  // Handle the 'onload' event of our XHR request, generated in 'requestBees',
-  // by generating 'img' elements, and stuffing them into the document for 
-  // display.
-  showPhotos_: function(e) {
-    var bees = e.target.responseXML.querySelectorAll('photo');
-    for(var i = 0; i < bees.length; i++) {
-      var img = document.createElement('img');
-      img.src = this.constructBeeURL_(bees[i]);
-      img.setAttribute('alt', bees[i].getAttribute('title'));
-      document.body.appendChild(img);
-    }
-  },
+	// Sends an XHR GET request to grab photos. The XHR's 'onload' event is 
+	// hooked up to the 'showPhotos_' method.
+	requestUser: function() {
+	},
 
-  // Given a photo, construct a URL...
-  constructBeeURL_: function(photo) {
-    return "http://farm" + photo.getAttribute("farm") +
-           ".static.flickr.com/" + photo.getAttribute("server") +
-           "/" + photo.getAttribute("id") +
-           "_" + photo.getAttribute("secret") +
-           "_s.jpg";
-  }
+	requestBees: function() {
+		var req = new XMLHttpRequest();
+		req.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				beeGenerator.showGoals_(this.responseText);
+			}
+		};
+		req.open("GET", this.requestDangerGoals_, true);
+		//req.onload = this.showGoals_.bind(this);
+		req.send(null);
+	},
+
+	// Handle the 'onload' event of our XHR request, generated in 'requestBees',
+	// by generating 'img' elements, and stuffing them into the document for 
+	// display.
+	showGoals_: function(e) {
+
+		var data = JSON.parse(e);
+
+		var goals = data.goals;
+
+		for (var i = 0; i < goals.length; i++) {
+			var goal = document.createElement("p");
+			goal.textContent = goals[i];
+			document.body.appendChild(goal);
+		}
+
+		// var bees = e.target.responseXML.querySelectorAll('photo');
+		// for(var i = 0; i < bees.length; i++) {
+		//   var img = document.createElement('img');
+		//   img.src = this.constructBeeURL_(bees[i]);
+		//   img.setAttribute('alt', bees[i].getAttribute('title'));
+		//   document.body.appendChild(img);
+		// }
+	},
+
+	// Given a photo, construct a URL...
+	constructBeeURL_: function(photo) {
+		return "http://farm" + photo.getAttribute("farm") +
+			".static.flickr.com/" + photo.getAttribute("server") +
+			"/" + photo.getAttribute("id") +
+			"_" + photo.getAttribute("secret") +
+			"_s.jpg";
+	}
 };
 
 // Run bee generation script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function() {
-  beeGenerator.requestBees();
+	beeGenerator.requestBees();
 });
